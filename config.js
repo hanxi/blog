0d7cfc1affcb67bd6412269f7d00e313cfc07427
涵曦 <hanxi.info@gmail.com>
2015-04-25 00:26:14 +0800

    add config file
    整理代码

---
+++ config.js
@@ -0,0 +1,4 @@
+config = {
+	blog_name: "涵曦 ● 博客",
+	github_repo: "hanxi/blog"
+};
--- util.js
+++ util.js
@@ -14,9 +14,9 @@ getJSON = function(url, callback)
 
 ajax = function(post, url, callback) {
 	var xhr = new window.XMLHttpRequest();
-	var method = "get";
+	var method = "post";
 	if (post == null) {
-		method = "post";
+		method = "get";
 	}
 	xhr.open(method, url, true);
 	xhr.setRequestHeader("Accept", "application/json, text/javascript, */*; q=0.01");
@@ -27,4 +27,45 @@ ajax = function(post, url, callback) {
 		}
 	}
 	xhr.send(JSON.stringify(post));
+}
+
+renderMarkdown = function(content,text)
+{
+	var post = {
+		"text": text,
+		"mode": "gfm",
+	};
+	ajax(post, "https://api.github.com/markdown", function(data) {
+		content.innerHTML = data;
+	});
+}
+
+getPageUrl = function()
+{
+	return "https://api.github.com/repos/"+config.github_repo+"/issues";
+}
+
+getIssuesUrl = function(id)
+{
+	return "https://github.com/"+config.github_repo+"/issues/"+id+"#new_comment_field";
+}
+
+setTitle = function(title)
+{
+	var titles = document.getElementsByTagName("title");
+	titles[0].innerHTML = title;
+}
+
+setBlogName = function()
+{
+    var btitle = document.getElementById("blog_title");
+    var txt = document.createTextNode(config.blog_name);
+    btitle.appendChild(txt);
+}
+
+cleanChild = function(node)
+{
+	while (node.firstChild) {
+		node.removeChild(node.firstChild);
+	}
 }
