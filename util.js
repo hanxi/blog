207f17a67f8682396b670996a19e2172774e86c8
涵曦 <hanxi.info@gmail.com>
2015-04-24 20:02:05 +0800

    fuck

--- util.js
+++ util.js
@@ -11,3 +11,20 @@ getJSON = function(url, callback)
 	}
 	xhr.send();
 }
+
+ajax = function(post, url, callback) {
+	var xhr = new window.XMLHttpRequest();
+	var method = "get";
+	if (post == null) {
+		method = "post";
+	}
+	xhr.open(method, url, true);
+	xhr.setRequestHeader("Accept", "application/json, text/javascript, */*; q=0.01");
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState==4 && xhr.status==200)
+		{
+			callback(xhr.responseText);
+		}
+	}
+	xhr.send(JSON.stringify(post));
+}
