1ab443662029f001381d24508c9acda1ace567c2
涵曦 <hanxi.info@gmail.com>
2015-04-26 02:16:50 +0800

    clean code

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ config = {
     blog_name: "涵曦 ● 博客",
     github_username: "hanxi", // github 账号名
     github_repo: "blog",      // github 建立的 repo 名
-    per_page: 10,              // 首页每次载入文章列表数量
+    per_page: 10,             // 首页每次载入文章列表数量
 };
