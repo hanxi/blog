f874428c88c54e65467ca7eab2a752e2ca47620e
涵曦 <hanxi.info@gmail.com>
2015-06-18 09:04:33 +0800

    add access_token

--- config.js
+++ config.js
@@ -3,4 +3,5 @@ config = {
     github_username: "hanxi", // github 账号名
     github_repo: "blog",      // github 建立的 repo 名
     per_page: 10,             // 首页每次载入文章列表数量
+    access_token: "8e434296090136b1b4c9b8830e74cd13943bd2f2" // 请求量大时需要在 github 后台单独设置一个读取公开库的 token
 };
