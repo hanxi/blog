c527219a9348687b0cd197026a622260004e7cdc
涵曦 <hanxi.info@gmail.com>
2015-06-18 09:44:39 +0800

    fuck the access_token.

--- config.js
+++ config.js
@@ -5,6 +5,5 @@ config = {
     per_page: 10,             // 首页每次载入文章列表数量
 
     // 请求量大时需要在 github 后台单独设置一个读取公开库的 token
-    access_token_1: "dbfe3dd66ee904988f2b",
-    access_token_2: "aaaf9bf49a03b6e3fd62"
+    access_token: "dbfe3dd66ee904988f2b"+"aaaf9bf49a03b6e3fd62"
 };
