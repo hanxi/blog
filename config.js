9a9b324cc3f4fc57cb2fed65f818c4f49cc425c4
涵曦 <hanxi.info@gmail.com>
2019-10-11 09:50:31 +0800

    Update config.js

--- config.js
+++ config.js
@@ -5,5 +5,5 @@ config = {
     per_page: 10,             // 首页每次载入文章列表数量
 
     // 请求量大时需要在 github 后台单独设置一个读取公开库的 token
-    access_token: "dbfe3dd66ee904988f2b"+"aaaf9bf49a03b6e3fd62"
+    access_token: "d229952b3e850117931"+"e15ba7e5db4939d346f0b"
 };
