2abdd39373be7266a70afea8d8260d9c1305cd25
涵曦 <hanxi.info@gmail.com>
2020-02-06 04:42:21 +0800

    use the Authorization HTTP header
    Please use the Authorization HTTP header instead, as using the `access_token` query parameter is deprecated.
    
    Depending on your API usage, we'll be sending you this email reminder once every 3 days for each token and User-Agent used in API calls made on your behalf.
    Just one URL that was accessed with a token and User-Agent combination will be listed in the email reminder, not all.
    
    Visit https://developer.github.com/changes/2019-11-05-deprecated-passwords-and-authorizations-api/#authenticating-using-query-parameters for more information.

--- ajax.js
+++ ajax.js
@@ -44,6 +44,7 @@ ajax = function(post, url, callback) {
     }
     xhr.open(method, url, true);
     xhr.setRequestHeader("Accept", "application/json, text/javascript, */*; q=0.01");
+    xhr.setRequestHeader("Authorization", "token " + config.access_token);
     xhr.onreadystatechange = function() {
         if (xhr.readyState==4 && xhr.status==200)
         {
